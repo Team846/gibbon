5f4a7b31c094df5bd7080c66fa1828fe1c20efa6
rh3as <rheasusarla@gmail.com>
2026-03-03 17:41:29 -0800

    Blue/red test fixed and changed back clang version

--- src/test/cpp/shooting_calculator_test.cc
+++ src/test/cpp/shooting_calculator_test.cc
@@ -5,26 +5,26 @@
 using namespace pdcsu::units;
 
 TEST(ShootingCalculatorTest, RedBlueMirrorKeepsValidityAndSpeedClose) {
-    ShootingCalculator::Setup();
+  ShootingCalculator::Setup();
 
-    ShootingCalculatorInput red{
-        .position = {100_in_, 200_in_}, //just put random numbers here for now
-        .velocity = {2.0_fps_, 1.0_fps_},
-        .bearing = 20_deg_, 
-        .yaw_rate = 0_degps_,
-        .is_blue_alliance = false};
-    
-    ShootingCalculatorInput blue = red;
-    blue.position = {funkit::math::FieldPoint::field_size_x - red.position[0],
-            funkit::math::FieldPoint::field_size_y - red.position[1]};
-    blue.bearing = red.bearing + 180_deg_;
-    blue.is_blue_alliance = true;
+  ShootingCalculatorInput red{
+      .position = {100_in_, 200_in_},  // just put random numbers here for now
+      .velocity = {2.0_fps_, 1.0_fps_},
+      .bearing = 20_deg_,
+      .yaw_rate = 0_degps_,
+      .is_blue_alliance = false};
 
-    ShootingCalculatorOutputs out_red = ShootingCalculator::CalculateFromInput(red);
-    ShootingCalculatorOutputs out_blue = 
-        ShootingCalculator::CalculateFromInput(blue);
-    
-    EXPECT_EQ(out_red.is_valid, out_blue.is_valid);
-    EXPECT_NEAR(out_red.shooter_vel.value(), out_blue.shooter_vel.value(), 0.5);
+  ShootingCalculatorInput blue = red;
+  blue.position = {funkit::math::FieldPoint::field_size_x - red.position[0],
+      funkit::math::FieldPoint::field_size_y - red.position[1]};
+  blue.bearing = red.bearing + 180_deg_;
+  blue.is_blue_alliance = true;
 
+  ShootingCalculatorOutputs out_red =
+      ShootingCalculator::CalculateFromInput(red);
+  ShootingCalculatorOutputs out_blue =
+      ShootingCalculator::CalculateFromInput(blue);
+
+  EXPECT_EQ(out_red.is_valid, out_blue.is_valid);
+  EXPECT_NEAR(out_red.shooter_vel.value(), out_blue.shooter_vel.value(), 0.5);
 }
