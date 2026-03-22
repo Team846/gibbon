e9c825492ea98ad5ac456a21cbfa45212807dbb1
Rayming <raymingye93@gmail.com>
2026-03-22 10:05:48 -0700

    changes?

--- src/yearly/cpp/commands/teleop/speed_test_command.cc
+++ src/yearly/cpp/commands/teleop/speed_test_command.cc
@@ -14,7 +14,7 @@ void SpeedTestCommand::Periodic() {
   funkit::robot::swerve::DrivetrainTarget target{};
   target.velocity = {0_fps_,
       container_.drivetrain_.GetPreferenceValue_unit_type<pdcsu::units::fps_t>(
-          "speed_test_command/max_velocity")};  // hardcoded max speed?
+          "max_speed")};  // hardcoded max speed?
   target.angular_velocity = pdcsu::units::degps_t{0};
 
   container_.drivetrain_.SetTarget(target);
@@ -28,8 +28,9 @@ void SpeedTestCommand::OnEnd(bool interrupted) {
 }
 
 bool SpeedTestCommand::IsFinished() {
+  std::cout << container_.drivetrain_.GetReadings().pose.velocity.magnitude().value() << std::endl;
   return (
-      container_.drivetrain_.GetReadings().pose.velocity.magnitude() >
+      container_.drivetrain_.GetReadings().pose.velocity.magnitude() >=
       container_.drivetrain_.GetPreferenceValue_unit_type<pdcsu::units::fps_t>(
           "speed_test_command/max_velocity"));  // hardcoded max speed?
 }
